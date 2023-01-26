177ec7fd9bb41a3bc8df83f70c2badade53a87ad
RicLMCR <99919289+RicLMCR@users.noreply.github.com>
2023-01-26 13:39:27 +0000

    styling changes

--- kuberapp/src/components/list/list.js
+++ kuberapp/src/components/list/list.js
@@ -2,11 +2,12 @@ const List = ({ list }) => {
   return (
     <div className="list">
       <h2>Sample List</h2>
+
       {list.map((lists) => (
         <div className="listEntry" key={lists.key}>
-          <li>
-            <ul>{lists.name}</ul>
-          </li>
+          <ul>
+            <li>{lists.name}</li>
+          </ul>
         </div>
       ))}
     </div>
